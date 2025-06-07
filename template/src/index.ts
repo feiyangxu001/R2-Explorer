6b866d6fbb6563b8e4f778fd44603319d54aa70e
luffy <luffy.xu@zoom.us>
2025-06-06 23:02:50 -0700

    update basic auth

--- template/src/index.ts
+++ template/src/index.ts
@@ -2,9 +2,10 @@ import { R2Explorer } from "r2-explorer";
 
 // Read all available configurations here: https://r2explorer.com/getting-started/configuration/
 export default R2Explorer({
-	readonly: true,
+	readonly: false,
 	basicAuth: {
 		username: "luffy",
 		password: "luffy,.894684862",
 	},
 });
+
